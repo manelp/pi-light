f175e0c53920c59968965edee5fc2a61a45ac78c
Manel P <manel@perezbondia.com>
2015-02-05 09:31:45 +0100

    Export pin on start

--- app.js
+++ app.js
@@ -8,23 +8,19 @@ app.set('port', process.env.PORT || 3000);
 
 app.use('/', express.static(__dirname + '/public'));
 
+//export pin
+(function() {	
+	gpio.open(16, "out", function(err) {});
+})();
+
 app.post('/api/on', function(req, res){
 	async.series([
-		function(callback) {
-			// Open pin for output
-			gpio.open(16, "output", callback);
-		},
 		function(callback) {
 			// Turn the relay on
 			gpio.write(16, 0, callback);
 		},
 		function(err, results) {
-			setTimeout(function() {
-				// Close pin from further writing
-				gpio.close(16);
-				// Return json
-				res.json("ok");
-			}, 500);
+			res.json("ok");
 		}
 	]);
 });
@@ -32,20 +28,11 @@ app.post('/api/on', function(req, res){
 app.post('/api/off', function(req, res){
 	async.series([
 		function(callback) {
-			// Open pin for output
-			gpio.open(16, "output", callback);
-		},
-		function(callback) {
-			// Turn the relay on
+			// Turn the relay off
 			gpio.write(16, 1, callback);
 		},
 		function(err, results) {
-			setTimeout(function() {
-				// Close pin from further writing
-				gpio.close(16);
-				// Return json
-				res.json("ok");
-			}, 500);
+			res.json("ok");
 		}
 	]);
 });
